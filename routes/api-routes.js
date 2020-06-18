0f29f321a70a26463d91b4002908644e5739f767
Erika Vigliucci <evigliucci@gmail.com>
2020-06-17 21:36:48 -0400

    Final bug fixes

---
+++ routes/api-routes.js
@@ -0,0 +1,32 @@
+var db = require("../models");
+
+module.exports = function(app) {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        });
+
+        app.get("/api/workouts/range", (req, res) => {
+            db.Workout.find({}).then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+        })
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(req.params.id, {
+            $push: {
+                exercises: req.body
+            }
+        }).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body).then(success => {
+            res.json(success);
+        })
+    })
+
+};
---
+++ routes/html-routes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+    app.get("/stats", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    })
+    app.get("/exercise", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    })
+};
